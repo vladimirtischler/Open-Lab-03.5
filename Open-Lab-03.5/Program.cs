845c09312133a670ae01a18f4968bd8944c15bd8
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-07 08:34:00 +0100

    gfdb

---
+++ Open-Lab-03.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Open_Lab_03._5
+{
+    class Program
+    {
+        public static bool MatchCaseInsensitive(string a, string b)
+        {
+            return(a.ToLower() == b.ToLower());
+        }
+
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            Console.WriteLine(MatchCaseInsensitive(name1, name2));
+
+        }
+    }
+}
